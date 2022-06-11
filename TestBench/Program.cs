f9cebb7e01eb382b02bbe2bd35ea80bbb40eb19d
Leardo Taylor <leardotaylor@gmail.com>
2022-06-11 21:27:06 +0200

    init

---
+++ TestBench/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for morinformation
+Console.WriteLine("Hello, World!");
+
+int number1 = 4, number2 = 2, number3 = 7, number4 = 5;
+
+int ans1 = number1 + number2, ans2 = number3 - number4;
+int ultimate = number1 + number2 * number3 - number4;
+
+Console.WriteLine("Goodbye, World!");
